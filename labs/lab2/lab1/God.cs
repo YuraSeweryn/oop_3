326af1b530005830495f00977a4cffaf441d1d2c
Yurii Seweryn <yurik.seweryn@gmail.com>
2021-10-25 10:02:23 +0300

    upd don

---
+++ labs/lab2/lab1/God.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab1
+{
+    sealed class God : HalfGod
+    {
+        bool HaveCreatedWorld;
+        static God CreateOurGod(string confirmation)
+        {
+            if(confirmation == "Create God")
+            {
+                return new God("GOD", true);
+            }
+
+            throw new AccessViolationException("Wrong confirmation password!");
+        }
+        God(string name, bool b)
+        {
+            HaveCreatedWorld = b;
+        }
+
+        public God(string name) : base(name)
+        {
+            HaveCreatedWorld = false;
+        }
+
+        public string IsOurGod()
+        {
+            if (HaveCreatedWorld)
+            {
+                return "I'm God, I've created world";
+            }
+            return "I haven't created world(";
+        }
+
+
+        ~God()
+        {
+            Dispose(disposing: false);
+            Console.WriteLine("God destructed");
+        }
+
+    }
+}
---
+++ labs/lab2/lab1/SuperHero.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    class SuperHero : Hero
+    {
+        public List<string> SuperPowers { get; private set; }
+
+        #region Constructor
+        public SuperHero()
+        {
+            counter++;
+        }
+        public SuperHero(string name, List<string> superPowers) : base(name, default)
+        {
+            SuperPowers = superPowers;
+        }
+        #endregion
+
+
+        public override string SuperPower()
+        {
+            string answ = "My superpowers are:";
+            foreach(var a in SuperPowers)
+            {
+                answ += $"- {a}\n";
+            }
+            return answ;
+        }
+
+        ~SuperHero()
+        {
+            Dispose(disposing: false);
+            Console.WriteLine("SuperHero destructed");
+        }
+    }
+}
